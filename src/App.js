1a9d84fcefc40ad9f4ddfd5e39ce301a0486282c
takeseem <takeseem@gmail.com>
2024-10-08 10:47:48 +0800

    增加 kanban-board 和 column UI

--- src/App.js
+++ src/App.js
@@ -8,6 +8,35 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>我的看板</h1>
       </header>
+      <main className="kanban-board">
+        <section className='kanban-column column-todo'>
+          <h2>待办事项</h2>
+          <ul>
+            <li>
+              <div>任务-1</div>
+              <div>2024-10-08 10:35</div>
+            </li>
+          </ul>
+        </section>
+        <section className='kanban-column column-ongoing'>
+          <h2>进行中</h2>
+          <ul>
+            <li>
+              <div>任务-1</div>
+              <div>2024-10-08 10:35</div>
+            </li>
+          </ul>
+        </section>
+        <section className='kanban-column column-done'>
+          <h2>已完成</h2>
+          <ul>
+            <li>
+              <div>任务-1</div>
+              <div>2024-10-08 10:35</div>
+            </li>
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
