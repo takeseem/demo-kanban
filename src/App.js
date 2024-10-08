6064e7d3247d2124eec14df3fcfd56001db2d165
takeseem <takeseem@gmail.com>
2024-10-08 16:22:14 +0800

    抽取 KanbanCardList 组件

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const todoList = [
+    { id: 1, title: '任务-1', time: '2024-10-08 10:35' },
+    { id: 3, title: '任务-3', time: '2024-10-08 10:35' },
+    { id: 3, title: '任务-5', time: '2024-10-08 10:35' },
+  ];
+  const ongoingList = [
+    { id: 1, title: '任务-4', time: '2024-10-08 10:35' },
+    { id: 2, title: '任务-2', time: '2024-10-08 10:35' },
+    { id: 3, title: '测试-2', time: '2024-10-08 10:35' },
+  ];
+  const doneList = [
+    { id: 1, title: '任务-2', time: '2024-10-08 10:35' },
+    { id: 3, title: '测试-1', time: '2024-10-08 10:35' },
+  ];
+  const KanbanCard = ({ title, time }) => (
+    <li className='kanban-card'>
+      <div className='card-title'>{title}</div>
+      <div className='card-time'>{time}</div>
+    </li>
+  );
+  const KanbanCardList = ({ list }) => list.map(item => <KanbanCard {...item} />);
   return (
     <div className="App">
       <header className="App-header">
@@ -12,36 +33,19 @@ function App() {
         <section className='kanban-column column-todo'>
           <h2>待办事项</h2>
           <ul>
-            <li className='kanban-card'>
-              <div className='card-title'>任务-1</div>
-              <div className='card-time'>2024-10-08 10:35</div>
-            </li>
-            <li className='kanban-card'>
-              <div className='card-title'>任务-1</div>
-              <div className='card-time'>2024-10-08 10:35</div>
-            </li>
-            <li className='kanban-card'>
-              <div className='card-title'>任务-1</div>
-              <div className='card-time'>2024-10-08 10:35</div>
-            </li>
+            <KanbanCardList list={todoList} />
           </ul>
         </section>
         <section className='kanban-column column-ongoing'>
           <h2>进行中</h2>
           <ul>
-            <li className='kanban-card'>
-              <div className='card-title'>任务-1</div>
-              <div className='card-time'>2024-10-08 10:35</div>
-            </li>
+            <KanbanCardList list={ongoingList} />
           </ul>
         </section>
         <section className='kanban-column column-done'>
           <h2>已完成</h2>
           <ul>
-            <li className='kanban-card'>
-              <div className='card-title'>任务-1</div>
-              <div className='card-time'>2024-10-08 10:35</div>
-            </li>
+            <KanbanCardList list={doneList} />
           </ul>
         </section>
       </main>
